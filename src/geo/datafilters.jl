aca963c9519843f5314004ad480a77ce32688029
Sebastien Martin <semartin@mit.edu>
2016-02-23 19:45:21 -0500

    added geoTimings type

--- src/geo/datafilters.jl
+++ src/geo/datafilters.jl
@@ -1,41 +1,7 @@
 ###################################################
-## geo/geodata.jl
-## Representing trip data with continuous geographical locations
+## geo/datafilters.jl
+## Filtering geo-trips
 ###################################################
-"""
-    `GeoTrip` : data for one geographical trip
-"""
-immutable GeoTrip
-    "pick-up lonlat coordinates"
-    pLon::Float32
-    pLat::Float32
-
-    "drop-off lonlat coordinates"
-    dLon::Float32
-    dLat::Float32
-
-    "Pick-up date and time"
-    pTime::DateTime
-
-    "Trip time (seconds)"
-    time::Float32
-end
-function Base.show(io::IO, t::GeoTrip)
-    print(io,"$(t.pTime) -- ($(t.pLon),$(t.pLat))=>($(t.dLon),$(t.dLat)): $(tripDistance(t))m in $(t.time)s ")
-end
-
-"""
-    `GeoData`: list of `GeoTrip`
-"""
-typealias GeoData Vector{GeoTrip}
-
-"""
-    `stats`: print information
-"""
-function stats(t::GeoData)
-    println("GeoData: trip information in GPS coordinates")
-    println("--- $(length(t)) trips: $(sizeof(t)/1e6)MB")
-end
 
 """
     `removeBadTrips`: filter a `GeoTrip` list to remove outliers
