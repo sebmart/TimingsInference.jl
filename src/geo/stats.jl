f043c3bf0a4b972b62058944a70707abe8745246
Arthur Delarue <adelarue@mit.edu>
2016-05-15 23:42:17 -0400

    Fixed ectra parenthesis and stat formatting

--- src/geo/stats.jl
+++ src/geo/stats.jl
@@ -46,7 +46,7 @@ type RealGeoStats <: GeoStats
 			"testTripsLogErrorbt" => 100 * testTripsLogErrorByTime(gt, ds, obj.timeBound),
 			"trTripsLogErrorbt" => 100 * trTripsLogErrorByTime(gt, ds, obj.timeBound),
 			"testTripsBiasbt" => testTripsBiasByTime(gt, ds, obj.timeBound),
-			"trTripsBiasbt" => trTripsBiasByTime(gt, ds, obj.timeBound)),
+			"trTripsBiasbt" => trTripsBiasByTime(gt, ds, obj.timeBound),
 			"testTripsLogBiasbt" => testTripsLogBiasByTime(gt, ds, obj.timeBound),
 			"trTripsLogBiasbt" => trTripsLogBiasByTime(gt, ds, obj.timeBound))
 		return obj
@@ -64,13 +64,13 @@ function printStats(so::RealGeoStats; outputFileName = "")
 			if contains(statName, "bt")
 				println(statName, ":")
 				for (i,time) in enumerate(so.timeBound)
-					if contains(lowercase(statName), "bias")
+					if contains(lowercase(statName), "bias") && !contains(lowercase(statName), "log")
 						@printf("\t\tTime < %.0f s:\t%.0fs\n", time, so.sdict[statName][i])
 					else
 						@printf("\t\tTime < %.0f s:\t%.2f%%\n", time, so.sdict[statName][i])
 					end
 				end
-			elseif contains(lowercase(statName), "bias")
+			elseif contains(lowercase(statName), "bias") && !contains(lowercase(statName), "log")
 				print(statName, ":\t")
 				@printf("%.2f s\n", so.sdict[statName])
 			else
@@ -84,13 +84,13 @@ function printStats(so::RealGeoStats; outputFileName = "")
 		for statName in sort(collect(keys(so.sdict)))
 			if contains(statName, "bt")
 				for (i,time) in enumerate(so.timeBound)
-					if contains(lowercase(statName), "bias")
+					if contains(lowercase(statName), "bias") && !contains(lowercase(statName), "log")
 						@printf(f, "\t\tTime < %.0f s:\t%.0fs\n", time, so.sdict[statName][i])
 					else
 						@printf(f, "\t\tTime < %.0f s:\t%.2f%%\n", time, so.sdict[statName][i])
 					end
 				end
-			elseif contains(lowercase(statName), "bias")
+			elseif contains(lowercase(statName), "bias") && !contains(lowercase(statName), "log")
 				write(f, string(statName, ":\t"))
 				@printf(f, "%.0fs\n", so.sdict[statName])
 			else
@@ -130,14 +130,14 @@ function printStats(stats::Vector{RealGeoStats}, statName::AbstractString)
 		if contains(statName, "bt")
 			print(so.name, "\t")
 			for (i,time) in enumerate(so.timeBound)
-				if contains(lowercase(statName), "bias")
+				if contains(lowercase(statName), "bias") && !contains(lowercase(statName), "log")
 					@printf("%.0fs\t", so.sdict[statName][i])
 				else
 					@printf("%.2f%%\t", so.sdict[statName][i])
 				end
 			end
 			print("\n")
-		elseif contains(lowercase(statName), "bias")
+		elseif contains(lowercase(statName), "bias") && !contains(lowercase(statName), "log")
 			@printf("%s\t\t%.0fs\n", so.name, so.sdict[statName])
 		else
 			@printf("%s\t\t%.2f%%\n", so.name, so.sdict[statName])
