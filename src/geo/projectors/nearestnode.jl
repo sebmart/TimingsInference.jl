8eaf65b59c64f7f28eccbc5d2aaf465f0c47eca2
Arthur Delarue <adelarue@mit.edu>
2016-06-16 13:31:00 -0400

    Corrected projection error

--- src/geo/projectors/nearestnode.jl
+++ src/geo/projectors/nearestnode.jl
@@ -82,10 +82,10 @@ function getNetworkTrips(nn::NearestNode, tIds::AbstractArray{Int64,1})
         end
         if haskey(netTrips, od)
             nt = netTrips[od]
-            time = nt.time * nt.weight/(nt.weight + 1.) + t.time/(nt.weight + 1.)
+            time = nt.time * nt.weight/(nt.weight + 1.) + odTime(t, od, nn.network)/(nt.weight + 1.)
             netTrips[od] = NetworkTrip(nt.orig, nt.dest, time, nt.weight+1.)
         else
-            netTrips[od] = NetworkTrip(od[1],od[2],t.time,1.)
+            netTrips[od] = NetworkTrip(od[1],od[2],odTime(t, od, nn.network),1.)
         end
     end
     return collect(values(netTrips))
@@ -96,7 +96,7 @@ end
 """
 function getTripTiming(nn::NearestNode, timings::NetworkTimings, tId::Int)
     o,d = nn.proj[tId]
-    return timings.pathTimes[o,d]
+    return tripTime(nn.trips[tId], (o,d), timings.pathTimes[o,d], nn.network)
 end
 function getTripTiming(nn::NearestNode, timings::NetworkTimings,
             pLon::Float32, pLat::Float32, dLon::Float32, dLat::Float32)
@@ -104,5 +104,43 @@ function getTripTiming(nn::NearestNode, timings::NetworkTimings,
     o = knn(nn.tree,[pX,pY],1)[1][1]
     dX, dY = toENU(dLon, dLat, nn.network)
     d = knn(nn.tree,[dX,dY],1)[1][1]
-    return timings.pathTimes[o,d]
+    return tripTime(pLon, pLat, dLon, dLat, (o,d), timings.pathTimes[o,d], nn.network)
+end
+
+"""
+    `odTime`: returns a time estimation for the travel time from o to d from the given trip by taking into account the projection distance/angle between proj. od and the trip
+"""
+function odTime(trip::GeoTrip, od::Tuple{Int, Int}, n::Network)
+    # calculate angles of projection and compute time accordingly
+    pX, pY = toENU(trip.pLon, trip.pLat, n)
+    dX, dY = toENU(trip.dLon, trip.dLat, n)
+    vec1 = [n.nodes[od[1]].x - pX, n.nodes[od[1]].y - pY]
+    vec2 = [dX - n.nodes[od[2]].x, dY - n.nodes[od[2]].y]
+    vecod = [n.nodes[od[2]].x - n.nodes[od[1]].x, n.nodes[od[2]].y - n.nodes[od[1]].y]
+    odTime = trip.time * (1 - (dot(vec1, vecod) + dot(vec2, vecod))/(tripDistance(trip) * norm(vecod)))
+    return odTime
+end
+
+"""
+    `tripTime`: returns  time estimation for the time of a trip based on the od that it projects to and the projection distance/angle
+"""
+function tripTime(trip::GeoTrip, od::Tuple{Int, Int}, pathTime::Float64, n::Network)
+    # calculate angles of projection and compute time accordingly
+    pX, pY = toENU(trip.pLon, trip.pLat, n)
+    dX, dY = toENU(trip.dLon, trip.dLat, n)
+    vec1 = [n.nodes[od[1]].x - pX, n.nodes[od[1]].y - pY]
+    vec2 = [dX - n.nodes[od[2]].x, dY - n.nodes[od[2]].y]
+    vecod = [n.nodes[od[2]].x - n.nodes[od[1]].x, n.nodes[od[2]].y - n.nodes[od[1]].y]
+    tripTime = pathTime * (1 + (dot(vec1, vecod) + dot(vec2, vecod))/(tripDistance(trip) * norm(vecod)))
+    return tripTime
+end
+function tripTime(pLon::Float32, pLat::Float32, dLon::Float32, dLat::Float32, od::Tuple{Int, Int}, pathTime::Float64, n::Network)
+    # calculate angles of projection and compute time accordingly
+    pX, pY = toENU(pLon, pLat, n)
+    dX, dY = toENU(dLon, dLat, n)
+    vec1 = [n.nodes[od[1]].x - pX, n.nodes[od[1]].y - pY]
+    vec2 = [dX - n.nodes[od[2]].x, dY - n.nodes[od[2]].y]
+    vecod = [n.nodes[od[2]].x - n.nodes[od[1]].x, n.nodes[od[2]].y - n.nodes[od[1]].y]
+    tripTime = pathTime * (1 + (dot(vec1, vecod) + dot(vec2, vecod))/(distanceGeo(pLon, pLat, dLon, dLat) * norm(vecod)))
+    return tripTime
 end
