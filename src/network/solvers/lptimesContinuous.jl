497a3c7b52132da447c294c2a95871f6cbd374c6
Arthur Delarue <adelarue@mit.edu>
2016-06-27 17:27:19 -0400

    Fixed huge number of variables

--- src/network/solvers/lptimesContinuous.jl
+++ src/network/solvers/lptimesContinuous.jl
@@ -13,21 +13,24 @@ function lpTimesContinuous(s::IterativeState, velocityBound::Float64 = 0.1; args
     tripData = s.trips
     roads = s.data.network.roads
 
+    println("Creating model")
     #Create the model (will be changed to avoid hard-coded parameters)
     # !BarConvTol needs to be changed
     m = Model(solver = GurobiSolver(TimeLimit=10000, Method=2, Crossover=0, BarConvTol=1e-8; args...))
 
+    println("Creating variables")
     # DECISION VARIABLES
     # Road times
     @defVar(m, t[i=vertices(g), j=out_neighbors(g,i)] >= s.data.minTimes[i,j])
     # Absolute difference between tripData times and computed times
     @defVar(m, epsilon[d=eachindex(tripData)] >= 0)
     # continuity vars
-    @defVar(m, velocity[i=vertices(g), j=out_neighbors(g,i), p=vertices(g), q=out_neighbors(g,p)] >= 0)
+    @defVar(m, velocity[(i,j,p,q) in flatten([[(src(edge), dst(edge), src(nearEdge), dst(nearEdge)) for nearEdge in findNearEdgesSameType(s.data.network, edge)] for edge in collect(edges(g))])] >= 0)
 
     # OBJECTIVE
     @setObjective(m, Min, sum{ sqrt(tripData[d].weight/tripData[d].time)*epsilon[d], d=eachindex(tripData)})
 
+    println("Creating constraints")
     # CONSTRAINTS
     # absolute values contraints (define epsilon), equal to time of first path
     @addConstraint(m, epsLower[d=eachindex(tripData)],
@@ -52,13 +55,13 @@ function lpTimesContinuous(s::IterativeState, velocityBound::Float64 = 0.1; args
             for nearEdge in findNearEdgesSameType(s.data.network, edge)
                 p=src(nearEdge); q=dst(nearEdge);
                 @addConstraint(m, t[i,j]/roads[i,j].distance - t[p,q]/roads[p,q].distance
-                    <= velocity[i,j,p,q])
+                    <= velocity[(i,j,p,q)])
                 @addConstraint(m, t[i,j]/roads[i,j].distance - t[p,q]/roads[p,q].distance
-                    >= -velocity[i,j,p,q])
+                    >= -velocity[(i,j,p,q)])
             end
         end
         vList = flatten([[(src(edge), dst(edge), src(nearEdge), dst(nearEdge)) for nearEdge in findNearEdgesSameType(s.data.network, edge)] for edge in cluster])
-        @addConstraint(m, sum{velocity[i,j,p,q], (i,j,p,q) in vList} <= length(vList) * velocityBound)
+        @addConstraint(m, sum{velocity[(i,j,p,q)], (i,j,p,q) in vList} <= length(vList) * velocityBound)
     end
 
     # SOLVE LP
