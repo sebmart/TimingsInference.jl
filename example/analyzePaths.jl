0753aebd1b9b5b628352519b9bb30399f1008811
Arthur Delarue <adelarue@mit.edu>
2016-07-04 16:41:54 -0400

    Updated visualization to work with incoming RoutingNetworks update

--- example/analyzePaths.jl
+++ example/analyzePaths.jl
@@ -4,6 +4,23 @@
 ###################################################
 
 using RoutingNetworks, TimingsInference, JLD;
-stats = load("/home/semartin/Tests/stats-randomPaths.jld", "nwstats");
-
+stats = load("Outputs/stats-randomPathsOriginal.jld", "nwstats");
+od = load("Outputs/nodePairsTotal.jld", "od");
 n = loadTemplate("Manhattan");
+
+tmpOD = Tuple{Int,Int}[];
+srand(1991);
+for (o,d) in shuffle(od)[1:10]
+	println(o, " => ", d)
+	push!(tmpOD, (o,d))
+end
+
+paths = load("Outputs/savedPathsVis.jld", "paths");
+
+ntv = collect(IntSet(TimingsInference.flatten(paths[6])));
+for (i,path) in enumerate(paths[6])
+    println(stats[i].name)
+    s = ShowPath(n, path)
+    s.nodesToView = n.nodes[ntv]
+    visualize(s)
+end
