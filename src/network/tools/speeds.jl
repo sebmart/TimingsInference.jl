fa092d078c6fe75ac60be29d1a05076351826dcd
Sebastien Martin <semartin@mit.edu>
2016-09-20 11:21:01 -0400

    bug fixes

--- src/network/tools/speeds.jl
+++ src/network/tools/speeds.jl
@@ -13,26 +13,27 @@ function typicalSpeeds(pb::NetworkData;
                        uniqueSpeed=false,
                        maxTrips=100_000)
     speeds = Float64[]
+    times = spzeros(Float64, nv(pb.network.graph), nv(pb.network.graph))
+
     initialTimes = randomTimes(pb.network);
 
     s = LimitedPaths(pb, initialTimes, pathsPerTrip = 1, maxTrip=maxTrips)
 
     logError = Inf
-    newLogError = nwTripsLogError(initialTimes, pb)
-    while newLogError <= logError
+    newLogError = nwTripsLogError(s.timings, pb)
+    while newLogError < logError
         # get the speeds
         speeds = constantSpeedSolver(s, uniqueSpeed=uniqueSpeed)
         # compute the times
-        times = spzeros(Float64, nv(g), nv(g))
-        for i in vertices(g), j in out_neighbors(g,i)
-            times[i,j] = roads[i, j].distance / speeds[roads[i ,j].roadType]
+        for i in vertices(pb.network.graph), j in out_neighbors(pb.network.graph,i)
+            times[i,j] = pb.network.roads[i, j].distance / speeds[pb.network.roads[i ,j].roadType]
         end
         # update the state
         updateState!(s, times)
 
         # compute new error
         logError = newLogError
-        newLogError = nwTripsLogError(times, pb)
+        newLogError = nwTripsLogError(s.timings, pb)
     end
     println("\n==============================================")
     println("Speeds converged, final error = $(newLogError)")
