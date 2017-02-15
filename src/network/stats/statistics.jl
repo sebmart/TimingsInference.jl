b4ca0b38627bcdf876513409cce5f35cf12dc3dd
Sebastien Martin <semartin@mit.edu>
2017-02-15 11:22:15 -0500

    inf error bis

--- src/network/stats/statistics.jl
+++ src/network/stats/statistics.jl
@@ -12,14 +12,20 @@ function tripsLogError(timings::NetworkTimings, trips::Vector{NetworkTrip})
     tt = getPathTimes(timings)
     times = timings.times
     error = 0.
+    count = 0
     for t in trips
         if t.roadProj
             error += t.weight * log(t.time / (tt[t.orig[2], t.dest[1]] + t.orig[3] * times[t.orig[1], t.orig[2]] + t.dest[3] * times[t.dest[1], t.dest[2]]))^2
         else
-            error += t.weight * log(t.time / tt[t.orig[2],t.dest[1]])^2
+            err = t.weight * log(t.time / tt2[t.orig[2],t.dest[1]])^2
+            if err < Inf
+                error += err
+            else
+                count += 1
+            end
         end
     end
-    return error/length(trips)
+    return error/(length(trips)-count)
 end
 
 """
@@ -31,14 +37,20 @@ function tripsLogBias(timings::NetworkTimings, trips::Vector{NetworkTrip})
     tt = getPathTimes(timings)
     times = timings.times
     bias = 0.
+    count = 0
     for t in trips
         if t. roadProj
             bias += t.weight * log(tt[t.orig[2], t.dest[1]] + t.orig[3] * times[t.orig[1], t.orig[2]] + t.dest[3] * times[t.dest[1], t.dest[2]]) - log(t.time)
         else
-            bias += t.weight * log(tt[t.orig[2], t.dest[1]]/t.time)
+            err = t.weight * log(tt[t.orig[2], t.dest[1]]/t.time)
+            if abs(err) < Inf
+                error += err
+            else
+                count += 1
+            end
         end
     end
-    return bias/length(trips)
+    return bias/(length(trips) - count)
 end
 
 """
